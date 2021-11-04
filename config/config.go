91e98e6e4b0f21b9261c604b334e9c8fcaea0bd7
Talkanbaev Artur <talkanbaev_a@auca.kg>
2021-11-04 22:23:04 +0600

    Adjusted config pkg and added docs Updated config file to make dsn work correctly

--- config/config.go
+++ config/config.go
@@ -4,26 +4,31 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v3"
 )
 
+//Main application configuration
 type AppConfig struct {
-	IsDevelopmentConfig bool `yaml:"is_dev"`
-	ServerConfig        struct {
-		Host    string `yaml:"host"`
-		Port    string `yaml:"port"`
-		Timeout struct {
+	IsDevelopmentConfig bool `yaml:"is_dev"` //Development environment indicator - implemented stupidly but works
+	// Http server configuration block. Has basic host and port params with default timeout values
+	ServerConfig struct {
+		Host    string   `yaml:"host"`
+		Port    string   `yaml:"port"`
+		Timeout struct { // Timeout struct, which includes default timeout values
 			Read  int
 			Write int
 		}
 	} `yaml:"server"`
+	// Database configuration block - used for DSN construction
 	DatabaseConfig struct {
 		Host            string
 		Port            string
-		User            string
-		Password        string
-		ConnectionLimit int
+		User            string // dev only username yaml field - in production it is read from env
+		Password        string // dev only password yaml field - in production it is read from env
+		Database        string
+		ConnectionLimit int `yaml:"connection_limit"`
 	} `yaml:"database"`
 }
 
@@ -31,14 +36,15 @@ var ErrNoConfigFile = errors.New("config file does not exist")
 var ErrFileIssue = errors.New("config file can not be opened")
 var ErrParseIssue = errors.New("configuration file can not be parsed")
 
-func ReadConfig(configPath *string) (*AppConfig, error) {
-	path := "./config/config.dev.yaml"
+//Reads configuration from hardcoded file or from path specified by env variable
+func ReadConfig() (*AppConfig, error) {
+	path, _ := filepath.Abs("./config/config.dev.yml") // make absolute for portability
 	if val, exists := os.LookupEnv("PROD_CONFIG"); exists {
-		path = val
+		path = val // if env is specified - forget about hardcoded value and use one from env instead
 	}
 
 	_, err := os.Stat(path)
-	if os.IsNotExist(err) {
+	if os.IsNotExist(err) { // check that file actually exists
 		return nil, ErrNoConfigFile
 	}
 
