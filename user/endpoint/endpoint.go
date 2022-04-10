d91a082fdb8ffeafb41b4adf4ad129e5f7dc2bf5
Talkanbaev Artur <talkanbaev_a@auca.kg>
2022-04-10 21:43:34 +0600

    Updated endpoints

--- user/endpoint/endpoint.go
+++ user/endpoint/endpoint.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-kit/kit/endpoint"
 	kitzap "github.com/go-kit/kit/log/zap"
+	"github.com/google/uuid"
 	"github.com/sea-auca/auc-auth/middleware"
 	"github.com/sea-auca/auc-auth/user/service"
 	"go.uber.org/zap"
@@ -16,7 +17,7 @@ type RegistrationRequest struct {
 }
 
 type RegistrationResponse struct {
-	Errors string `json:"err,omitempty"`
+	Errors string `json:"error,omitempty"`
 }
 
 func MakeRegistrationEndpoint(us service.UserService, lg *zap.Logger) endpoint.Endpoint {
@@ -28,6 +29,70 @@ func MakeRegistrationEndpoint(us service.UserService, lg *zap.Logger) endpoint.E
 		if err != nil {
 			response = RegistrationResponse{Errors: err.Error()}
 		}
+		return
+	}
+	e = middleware.LoggingMiddleware(logger)(e)
+	return e
+}
+
+type DeactivationRequest struct {
+	UserID uuid.UUID
+}
+
+func MakeDeactivationEndpoint(us service.UserService, lg *zap.Logger) endpoint.Endpoint {
+	logger := kitzap.NewZapSugarLogger(lg, zap.InfoLevel)
+	e := func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(DeactivationRequest)
+		user, err := us.GetUserByID(ctx, req.UserID)
+		if err != nil {
+			return
+		}
+		err = us.DeactivateAccount(ctx, user)
+		return
+	}
+	e = middleware.LoggingMiddleware(logger)(e)
+	return e
+}
+
+type ReactivationRequest struct {
+	Email string `json:"email"`
+}
+
+func MakeReactivationEndpoint(us service.UserService, lg *zap.Logger) endpoint.Endpoint {
+	logger := kitzap.NewZapSugarLogger(lg, zap.InfoLevel)
+	e := func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(ReactivationRequest)
+		err = us.ReactivateAccount(ctx, req.Email)
+		return
+	}
+	e = middleware.LoggingMiddleware(logger)(e)
+	return e
+}
+
+type ValidationRequest struct {
+	ID uuid.UUID
+}
+
+func MakeValidationEndpoint(us service.UserService, lg *zap.Logger) endpoint.Endpoint {
+	logger := kitzap.NewZapSugarLogger(lg, zap.InfoLevel)
+	e := func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(ValidationRequest)
+		err = us.ValidateUser(ctx, req.ID)
+		return
+	}
+	e = middleware.LoggingMiddleware(logger)(e)
+	return e
+}
+
+type ListUsersResponse struct {
+	Users []*service.User `json:"users"`
+}
+
+func MakeListUsersEndpoint(us service.UserService, lg *zap.Logger) endpoint.Endpoint {
+	logger := kitzap.NewZapSugarLogger(lg, zap.InfoLevel)
+	e := func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		users, _, err := us.ListUsers(ctx, 1, 100000)
+		response = ListUsersResponse{users}
 		return
 	}
 	e = middleware.LoggingMiddleware(logger)(e)
--- user/server/http.go
+++ user/server/http.go
@@ -3,11 +3,13 @@ package server
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	kitzap "github.com/go-kit/kit/log/zap"
 	"github.com/go-kit/kit/transport"
 	transp "github.com/go-kit/kit/transport/http"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/sea-auca/auc-auth/user/endpoint"
 	"github.com/sea-auca/auc-auth/user/service"
@@ -15,8 +17,11 @@ import (
 )
 
 type handlers struct {
-	regHandler    *transp.Server
-	verifyHandler *transp.Server
+	regHandler        *transp.Server
+	verifyHandler     *transp.Server
+	deactivateHandler *transp.Server
+	reactivateHandler *transp.Server
+	listHandler       *transp.Server
 }
 
 type errMsg struct {
@@ -44,7 +49,28 @@ func MakeHandlers(us service.UserService) handlers {
 	)
 
 	hands.verifyHandler = transp.NewServer(
-		endpoint.MakeEcho(us, lg),
+		endpoint.MakeValidationEndpoint(us, lg),
+		decodeVerifyRequest,
+		encodeResponse,
+		options...,
+	)
+
+	hands.deactivateHandler = transp.NewServer(
+		endpoint.MakeDeactivationEndpoint(us, lg),
+		decodeRequest,
+		encodeResponse,
+		options...,
+	)
+
+	hands.reactivateHandler = transp.NewServer(
+		endpoint.MakeReactivationEndpoint(us, lg),
+		decodeReactivateRequest,
+		encodeResponse,
+		options...,
+	)
+
+	hands.listHandler = transp.NewServer(
+		endpoint.MakeListUsersEndpoint(us, lg),
 		decodeRequest,
 		encodeResponse,
 		options...,
@@ -55,12 +81,37 @@ func MakeHandlers(us service.UserService) handlers {
 
 func RegisterRoutes(us service.UserService, r *mux.Router) {
 	hands := MakeHandlers(us)
-	r.Path("/api/register").Methods("POST").Handler(hands.regHandler)
-	r.Path("/api/verify").Methods("POST").Handler(hands.verifyHandler)
+	r.Path("/v1/register").Methods("POST").Handler(hands.regHandler)
+	r.Path("/v1/reactivate").Methods("POST").Handler(hands.reactivateHandler)
+	r.Path("/v1/deactivate").Methods("POST").Handler(hands.deactivateHandler)
+	r.Path("/v1/verify").Methods("POST").Handler(hands.verifyHandler)
+	r.Path("/v1/list").Methods("GET").Handler(hands.listHandler)
 }
 
 func decodeRequest(_ context.Context, r *http.Request) (interface{}, error) {
 	return nil, nil
+}
+
+func decodeReactivateRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request endpoint.ReactivationRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func decodeVerifyRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request endpoint.ValidationRequest
+	var err error
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		return nil, errors.New("malformed request, code has invalid format")
+	}
+	request.ID, err = uuid.Parse(code)
+	if err != nil {
+		return nil, errors.New("malformed request, code has invalid format")
+	}
+	return request, nil
 }
 
 func decodeRegisterRequest(_ context.Context, r *http.Request) (interface{}, error) {
@@ -72,5 +123,6 @@ func decodeRegisterRequest(_ context.Context, r *http.Request) (interface{}, err
 }
 
 func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(response)
 }
